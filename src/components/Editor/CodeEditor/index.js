9b784dbfbd6487457fda8f127953bb1ee411cab8
CloverHearts <cloverheartsdev@gmail.com>
2019-09-13 15:23:59 +0900

    Need to show log time in console #9

--- src/components/Editor/CodeEditor/index.js
+++ src/components/Editor/CodeEditor/index.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {chromeLight, Inspector} from 'react-inspector';
 import {Button, ButtonGroup, Position, Tooltip} from '@blueprintjs/core';
+import moment from 'moment';
 import * as monaco from 'monaco-editor';
 import './index.scss';
 
@@ -65,6 +66,9 @@ function LogView({Reporter}) {
       <div className="log-list">
         {logs.map((log, index) => (
           <div key={index} className={`log-row level-${log.level}`}>
+            <div className={`log-time`}>
+              {moment(log.time * 1000).calendar()}
+            </div>
             <Inspector
               theme={{...chromeLight, ...themes[log.level]}}
               data={log.data}
