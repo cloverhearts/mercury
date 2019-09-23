e6a5281c71d031f636bbcfb070175783559118b5
CloverHearts <cloverheartsdev@gmail.com>
2019-09-23 19:06:58 +0900

    Implement Task types

--- modules/core/index.js
+++ modules/core/index.js
@@ -1,9 +1,13 @@
-import Languages from './src/Code/Languages/Types'
-import CodeContainer from './src/Code/Container'
+import Languages from "./src/Code/Languages/Types";
+import CodeContainer from "./src/Code/Container";
+import TaskTypes from "./src/Task/Types";
 
 export default {
   Code: {
     Languages,
     Container: CodeContainer
+  },
+  Task: {
+    Types: TaskTypes
   }
-}
+};
---
+++ modules/core/src/Task/Types.js
@@ -0,0 +1,8 @@
+const TYPES = {
+  SYSTEM: {},
+  USER: {
+    CODE: "_code"
+  }
+};
+
+export default TYPES;
