8daec818764a3f13571ecc64cdea1f70edbee8d7
CloverHearts <cloverheartsdev@gmail.com>
2019-10-22 13:10:07 +0900

    IMPOROVE-31 NAV-BAR

--- src/components/Navigation/Service/index.js
+++ src/components/Navigation/Service/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { Alignment, Button, Navbar } from "@blueprintjs/core";
+import { NavLink } from "react-router-dom";
 
-export default (props) => {
+export default props => {
   return (
     <div>
-      <NavLink to="/" exact activeClassName="active">
-        Home
-      </NavLink>
-      <NavLink to="/informations" activeClassName="active">
-        Informations
-      </NavLink>
+      <div>
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>Mercury</Navbar.Heading>
+            <Navbar.Divider />
+            <NavLink to="/" exact activeClassName="active">
+              <Button className="bp3-minimal" icon="home" text="Home" />
+            </NavLink>
+            <NavLink to="/informations" activeClassName="active">
+              <Button className="bp3-minimal" icon="document" text="Informations" />
+            </NavLink>
+          </Navbar.Group>
+          <Navbar.Group align={Alignment.RIGHT}>
+            <Navbar.Divider />
+            <NavLink to="/" exact activeClassName="active">
+              <Button className="bp3-minimal" icon="cog" />
+            </NavLink>
+          </Navbar.Group>
+        </Navbar>
+      </div>
     </div>
   );
-}
+};
