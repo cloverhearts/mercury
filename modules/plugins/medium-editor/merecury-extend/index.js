62a198d5f3abebd0b174e688f0c6d8d14683f704
CloverHearts <cloverheartsdev@gmail.com>
2019-10-30 19:32:10 +0900

    IMPROVE-33 IMPORT Quilljs

--- modules/plugins/medium-editor/merecury-extend/index.js
+++
@@ -1,3 +0,0 @@
-import MercuryPlugin from './src/index'
-
-export default MercuryPlugin
--- modules/plugins/medium-editor/merecury-extend/tests/Htmldoc2Json.test.js
+++
@@ -1,4 +0,0 @@
-import doc from '../index'
-test('print test message', () => {
-  console.log(new doc().message())
-});
