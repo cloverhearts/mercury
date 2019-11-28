0661ec7a812f47e7db134736d5a77c957fa35f7d
CloverHearts <cloverheartsdev+github@gmail.com>
2019-11-29 03:01:45 +0900

    IMPROVE-44 Remove debug console

--- src/components/Note/Paragraph/CodeEditorContainer/Container/Render/index.js
+++ src/components/Note/Paragraph/CodeEditorContainer/Container/Render/index.js
@@ -17,9 +17,8 @@ export default function MercuryRenderContainer(props) {
         });
     }
   }, [renderRef.current]);
-  const onClick = (e) => console.log(Container);
   return <div id={`html-${Container.id}`} ref={renderRef}
-              className={`mercury-render-container`} onClick={onClick}>
+              className={`mercury-render-container`}>
     {Container.render && Container.render.html ? <div dangerouslySetInnerHTML={{ __html: Container.render.html }} /> : null}
   </div>;
 }
