b212d5c7dffaa51ef5f4d3b1dd960a13568e661d
CloverHearts <cloverheartsdev@gmail.com>
2020-01-06 16:11:10 +0900

    IMPROVE-77 Re-make npm module

--- modules/client/libraries/RequireModule/npm-programmatic.js
+++ modules/client/libraries/RequireModule/npm-programmatic.js
@@ -1,66 +1,47 @@
 const os = require('os');
-const {spawn} = require('child_process');
+const { exec } = require('child_process');
 const npmCommand = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
 
 module.exports = {
-  install: function (packages, opts) {
+  install: (packages, opts) => {
     return new Promise((resolve, reject) => {
       if (!packages || packages.length <= 0) {
         reject('No packages found');
       }
 
-      console.log(npmCommand, [
-        'install',
-        '--save',
-        ...packages].join(''))
-
-      const testForNpmCommand = spawn(npmCommand, [], {
+      exec(npmCommand, {
         shell: true,
-        stdio: 'inherit',
         cwd: opts.cwd ? opts.cwd : null
-      })
-
-      testForNpmCommand.on('stderr', (data) => {
-        if (typeof window !== 'undefined' && window._mercury &&
-          window._mercury.notification) {
-          window._mercury.notification.warn(
-            'Please install NodeJS\n(https://nodejs.org/en/download/), ' + npmCommand + ' not found');
-        }
-        reject(data);
-      });
-      
-      const executeNpm = spawn(npmCommand, [
-        'install',
-        '--save',
-        ...packages], {
-        env: {'NODE_TLS_REJECT_UNAUTHORIZED': '0'},
-        shell: true,
-        stdio: 'inherit',
-        cwd: opts.cwd ? opts.cwd : null
-      });
-      executeNpm.on('data', (data) => {
-        console.log(data.toString());
-        if (typeof window !== 'undefined' && window._mercury &&
-          window._mercury.notification) {
-          window._mercury.notification.log(data.toString());
-        }
-      });
-
-      executeNpm.on('stderr', (data) => {
-        reject(data);
-      });
-
-      executeNpm.on('exit', (code) => {
-        console.log(`Npm install child exited with code ${code}`);
-        if (!code) {
+      }, (error) => {
+        if (error) {
           if (typeof window !== 'undefined' && window._mercury &&
             window._mercury.notification) {
-            window._mercury.notification.error(
-              'Cannot found module ' + packages.join(' ') + ' on npm');
+            window._mercury.notification.warn(
+              'Please install NodeJS\n(https://nodejs.org/en/download/), ' + npmCommand + ' not found');
           }
+          reject('Please install NodeJS\n(https://nodejs.org/en/download/), ' + npmCommand + ' not found');
         }
-        resolve();
-      });
+
+        console.log(`execute command ${npmCommand} install --save ${packages.join(' ')}`)
+
+        exec(`${npmCommand} install --save ${packages.join(' ')}`, {
+          shell: true,
+          cwd: opts.cwd ? opts.cwd : null
+        }, (error, stdout, stderr) => {
+          if (error) {
+            if (typeof window !== 'undefined' && window._mercury &&
+              window._mercury.notification) {
+              window._mercury.notification.error(error);
+            }
+            reject(error)
+          }
+          if (typeof window !== 'undefined' && window._mercury &&
+            window._mercury.notification) {
+            window._mercury.notification.log(stdout);
+          }
+          resolve(stdout)
+        })
+      })
     });
   },
 };
